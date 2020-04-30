3a26e263457747393eae65fa4d0daddc950a06e4
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-04-30 16:21:33 +0530

    Fixed login

--- middleware/authFrontend.js
+++ middleware/authFrontend.js
@@ -1,25 +1,22 @@
-//const jwt = require('jsonwebtoken');
 const asyncHandler = require('./async');
 const { getMe } = require('../API/authRequests');
 
 exports.checkIfAuthenticated = asyncHandler(async (req, res, next) => {
-  console.log('Token is' + req.cookies['token']);
-  let token = req.cookies['token'];
-  if (!token) {
-    req.flash('error_message', 'Please log in to view that resouce');
-    res.redirect('/auth/login');
-  } else {
+  //console.log('Token is' + req.cookies['token']);
+  //let token = req.cookies['token'];
+  //if (!token) {
+   // req.flash('error_message', 'Please log in to view that resouce');
+   // res.redirect('/auth/login');
+ // } else {
     try {
       data = await getMe();
       req.user = data.data;
+      next();
     } catch (error) {
       console.log(error);
-      req.flash('error_message', 'Token invalid');
+      req.flash('error_message', 'Unauthorized to visit');
       res.redirect('/auth/login');
     }
 
-    next();
-  }
+  //}
 });
-
-// add redirect to dashboard here
