a18d69f0d9adac19c0915b89057961c05f05c3bd
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-04-30 23:27:59 +0530

    Fixed login and dashboard

--- server.js
+++ server.js
@@ -72,19 +72,11 @@ app.use(
 // Connect flash
 app.use(flash());
 
-// Global variables
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
+
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
 
-//mounting public directory
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Rate limiting
 const limiter = rateLimit({
@@ -102,6 +94,14 @@ app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.get('/', (req, res) => res.render('index'));
 app.get('/verify',(req,res) => res.render('verifyRecordAcceptance'))
