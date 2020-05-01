245e5ed72cf48cf938215e43b33b1affd97dbf67
varunhhhrahul <varunhhhrahul@gmail.com>
2020-05-01 20:10:04 +0530

    forented_changes

--- public/adminDashboard.js
+++ public/adminDashboard.js
@@ -2,16 +2,35 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 });
 
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
 function modshow(hospitalId)
 {
 	console.log(hospitalId);
 	var modalName=`.ui.basic.modal.${hospitalId}.first`
 	$(modalName).modal('show');
 }
-// $("").on("click",function(){
-//     $('.ui.basic.modal.mod1')
-//   .modal('show');
-// })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+  
+    var instances = M.Carousel.init(elems, {});
+  
+  });
 
 
 //below library is only used for encryption and decryption
--- public/userDashboardNext.js
+++ public/userDashboardNext.js
@@ -5,47 +5,20 @@ var addr
 
 anime.timeline({loop: false})
   .add({
-    targets: '.ml5 .line',
-    opacity: [0.5,1],
-    scaleX: [0, 1],
-    easing: "easeInOutExpo",
-    duration: 700
-  }).add({
-    targets: '.ml5 .line',
-    duration: 600,
-    easing: "easeOutExpo",
-    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
-  }).add({
-    targets: '.ml5 .ampersand',
+    targets: '.ml15 .word',
+    scale: [14,1],
     opacity: [0,1],
-    scaleY: [0.5, 1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
   }).add({
-    targets: '.ml5 .letters-left',
-    opacity: [0,1],
-    translateX: ["0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=300'
-  }).add({
-    targets: '.ml5 .letters-right',
-    opacity: [0,1],
-    translateX: ["-0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.ml5 .line',
-    opacity:0
-  }).add({
-    targets: '.ml5',
+    targets: '.ml15',
     opacity: 1,
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
   });
+  
 //below library is only used for encryption and decryption
 var EthCrypto = require('eth-crypto');
 
