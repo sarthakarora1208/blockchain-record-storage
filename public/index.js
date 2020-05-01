4ad262f8f61e45d336148a85a1b74e81fbbe3758
varunhhhrahul <varunhhhrahul@gmail.com>
2020-05-02 04:39:47 +0530

    added contact page

--- public/index.js
+++ public/index.js
@@ -13,6 +13,22 @@ $('.ui.fluid.dropdown').dropdown();
 $('.calc').calendar({
   type: 'date'
 });
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+ 
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
