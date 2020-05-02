c57f758dc77a4f2def8b27a99cfdf18a40428750
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-05-02 08:14:43 +0530

    Fixed server

--- middleware/authFrontend.js
+++ middleware/authFrontend.js
@@ -2,13 +2,20 @@ const asyncHandler = require('./async');
 const { getMe } = require('../API/authRequests');
 
 exports.checkIfAuthenticated = asyncHandler(async (req, res, next) => {
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+  }
+  console.log(localStorage.getItem('token'));
+
   //console.log('Token is' + req.cookies['token']);
   //let token = req.cookies['token'];
-  //if (!token) {
-   // req.flash('error_message', 'Please log in to view that resouce');
-   // res.redirect('/auth/login');
- // } else {
+  if (!token) {
+  req.flash('error_message', 'Please log in to view that resouce');
+    res.redirect('/auth/login');
+  } else {
     try {
+
       data = await getMe();
       req.user = data.data;
       next();
